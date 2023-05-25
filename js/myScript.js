0c373c6cec6b4a8daf01c8e254de6e3176f28ba3
Joane Vanesa <72459313+Joanevanesaa@users.noreply.github.com>
2023-05-25 14:54:00 +0700

    update

--- js/myScript.js
+++ js/myScript.js
@@ -1,6 +1,31 @@
-jQuery(function() {
-    jQuery(".sad").click(function() {
-        jQuery(".single").hide();
-        jQuery(".image" + $(this).attr('target')).show();
-    })
-})
+jQuery(function () {
+  jQuery(".button-med").click(function () {
+    jQuery(".single").hide();
+    jQuery(".video" + $(this).attr("target")).show();
+  });
+  
+  jQuery(".sad").click(function () {
+    jQuery(".single-s").hide();
+    jQuery(".image-s" + $(this).attr("target")).show();
+  });
+
+  jQuery(".anger").click(function () {
+    jQuery(".single-a").hide();
+    jQuery(".image-a" + $(this).attr("target")).show();
+  });
+
+  jQuery(".tense").click(function () {
+    jQuery(".single-t").hide();
+    jQuery(".image-t" + $(this).attr("target")).show();
+  });
+
+  jQuery(".relax").click(function () {
+    jQuery(".single-r").hide();
+    jQuery(".image-r" + $(this).attr("target")).show();
+  });
+
+  jQuery(".happy").click(function () {
+    jQuery(".single-h").hide();
+    jQuery(".image-h" + $(this).attr("target")).show();
+  });
+});
